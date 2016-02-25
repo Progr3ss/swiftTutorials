cb2974f1144d34bdcf5374c5a2e3997152df82cb
Martin Chibwe <mchibwe01@gmail.com>
2016-02-25 02:31:07 -0800

    qrReader to read any barcodes

---
+++ QRReader/QRReader/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  QRReader
+//
+//  Created by martin chibwe on 2/25/16.
+//  Copyright © 2016 martin chibwe. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+//struct Constants {
+//    
+//    
+//let  supportedBarCodes =
+//     [   AVMetadataObjectTypeQRCode, AVMetadataObjectTypeCode128Code, AVMetadataObjectTypeCode39Code, AVMetadataObjectTypeCode93Code, AVMetadataObjectTypeUPCECode, AVMetadataObjectTypePDF417Code, AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeAztecCode]
+//}
+let  supportedBarCodes =
+[  AVMetadataObjectTypeQRCode, AVMetadataObjectTypeCode128Code, AVMetadataObjectTypeCode39Code, AVMetadataObjectTypeCode93Code, AVMetadataObjectTypeUPCECode, AVMetadataObjectTypePDF417Code, AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeAztecCode]
